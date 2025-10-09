186009b8371441532bd5fcd077a4ddc9fdef477e
Preeti7447 <1107preetishah@gmail.com>
2025-10-09 13:54:19 +0530

    video updated

--- src/components/home/Video.jsx
+++ src/components/home/Video.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Video() {
   return (
     <div className='h-full w-full'>
-        <video className='h-full w-full object-cover overflow-hidden rounded' autoPlay loop muted src="public/videos/video-k27.mp4"></video>
+        <video className='h-full w-full object-cover overflow-hidden rounded' autoPlay loop muted src={`${import.meta.env.BASE_URL}videos/video-k27.mp4`}></video>
     </div>
   )
 }
